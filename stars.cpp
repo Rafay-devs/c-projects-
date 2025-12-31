4cde482e32456b11504d1738f948f1e5cafa7be8
Rafay-devs <abdulrafay.offical03@gmail.com>
2025-12-31 20:05:15 +0500

    Add files via upload

---
+++ stars.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,j ,sum=0;
+	for(i=5;i>=1;i--)
+	{	
+		for(int j=1;j<=i;j++)
+		{	
+		cout<<"*";
+		}
+	
+		
+	
+	cout<<endl;
+	}
+
+
+	return 0;
+}
