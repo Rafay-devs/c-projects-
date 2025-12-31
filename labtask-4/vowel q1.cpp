b1a33a5aed58b12abfcc033a85c7772bafb25977
Rafay-devs <abdulrafay.offical03@gmail.com>
2025-12-31 19:12:28 +0500

    vowel.cpp

---
+++ labtask-4/vowel q1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    if (ch == 'a' || ch == 'A' || ch == 'e' || ch == 'E' || ch == 'i' ||
+        ch == 'I' || ch == 'o' || ch == 'O' || ch == 'u' || ch == 'U') 
+		{
+        cout << ch << " is a vowel." << endl;
+    }
+	 else {
+        cout << ch << " is not a vowel." << endl;
+    }
+
+    return 0;
+}
+
--- vowel q1.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Dev-C++ with MinGW32 is working!" << endl;
-    return 0;
-}
